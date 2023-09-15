5aec72cd1ccc2af6f2ba0fbde1ebca6c20c9cb12
irsoft <ivanrsaiz@gmail.com>
2023-09-15 01:12:15 +0100

    Se eliminaron todas las interfaces y se modificó el archivo Flujo.java con las Interfaces Funcionales Estandar.

---
+++ src/main/java/com/irsoft/funcional/v7_interfaces_funcionales_estandar/Flujo.java
@@ -0,0 +1,66 @@
+package com.irsoft.funcional.v7_interfaces_funcionales_estandar;
+
+import com.irsoft.funcional.v7_interfaces_funcionales_estandar.interfaces.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.*;
+
+public class Flujo<T> {
+
+    private final List<T> valores;
+
+    public Flujo(List<T> valores) {
+        this.valores = valores;
+    }
+
+    public static <T> Flujo<T> proveer(int size, Supplier<T> proveedor) {
+        List<T> resultado = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            resultado.add(proveedor.get());
+        }
+        return new Flujo<>(resultado);
+    }
+
+    public Flujo<T> filtrar(Predicate<T> predicado) {
+        List<T> resultado = new ArrayList<>();
+        for (T valor : valores) {
+            if (predicado.test(valor)) resultado.add(valor);
+        }
+        return new Flujo<>(resultado);
+    }
+
+    public <R> Flujo<R> transformar(Function<T, R> function) {
+        List<R> resultado = new ArrayList<>();
+        for (T valor : valores) {
+            resultado.add(function.apply(valor));
+        }
+        return new Flujo<>(resultado);
+    }
+
+    public Flujo<T> actuar(Consumer<T> consumidor) {
+        for (T valor : valores) {
+            consumidor.accept(valor);
+        }
+        return new Flujo<>(valores);
+    }
+
+    public void consumir(Consumer<T> consumidor) {
+        for (T valor : valores) {
+            consumidor.accept(valor);
+        }
+    }
+
+    public T reducir(T identidad, BinaryOperator<T> funcionBinaria) {
+        T total = identidad;
+        for (T valor : valores) {
+            total = funcionBinaria.apply(total, valor);
+        }
+        return total;
+    }
+
+    @Override
+    public String toString() {
+        return valores.toString();
+    }
+}
---
+++ src/main/java/com/irsoft/funcional/v7_interfaces_funcionales_estandar/Main.java
@@ -0,0 +1,25 @@
+package com.irsoft.funcional.v7_interfaces_funcionales_estandar;
+
+import java.util.Random;
+
+public class Main {
+
+    Random random = new Random();
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    /**
+     * Constructor de Main
+     */
+    public Main() {
+
+        Integer total = Flujo.proveer(10, () -> random.nextInt(10))
+                .filtrar(valor -> valor % 2 == 0)
+                .transformar(valor -> valor * valor)
+                .actuar(valor -> System.out.println(valor))
+                .reducir(0, (val1, val2) -> val1 + val2);
+
+        System.out.println("Reducción: " + total);
+    }
+}
